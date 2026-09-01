40018109a55d833743e10816f2ff599b1faa0be3
Tasflex <gathunguignatius@gmail.com>
2026-09-01 10:33:51 +0300

    Updated with live links

--- app/layout.js
+++ app/layout.js
@@ -43,13 +43,6 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-Q3RR3WQL0R');
           `}
         </Script>
-
-
-{/* AvantLink Verification Script - Place in head */}
-        <Script
-          src="http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=148880a3354dc12096dbd27c86af507790fd8d27"
-          strategy="afterInteractive"
-        />
         
         <Header />
         <main className="flex-grow">
